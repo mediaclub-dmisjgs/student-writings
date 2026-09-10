d4d20d1af6d47f0e708818ca61cb24927a8a956b
Media Club of DMI St Joseph Global School <dmisjgs.media@gmail.com>
2026-09-10 22:02:57 +0530

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -4,7 +4,7 @@ const articles = [
   id: "must-i",
   title: "Must I?",
   category: "Poem",
-  image: "images/t-l-g.png",
+  image: "images/must-i.png",
   author: "Neiya Kin",
   grade: "Grade XII Science",
   date: "10 September 2026",
